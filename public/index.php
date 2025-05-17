bdf60098fccc4f5201086c817a5590b53de040f3
gardoffwhite <gardoffwhite@gmail.com>
2025-05-17 17:37:32 +0700

    Create index.php

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+header("Content-Type: text/xml; charset=UTF-8");
+$url = "http://nage-warzone.com/doc/rank1.php";
+$content = file_get_contents($url);
+
+// แปลงจาก TIS-620 → UTF-8
+$content_utf8 = iconv("TIS-620", "UTF-8//IGNORE", $content);
+
+echo $content_utf8;
+?>
